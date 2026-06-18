dc9b8332b595e6d9577c2660a24a2d08760f6307
Alex Testa <alex.testa@studio.unibo.it>
2026-06-18 10:08:07 +0200

    chore: init project using koltin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package com.github.alextesta00
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Hello, World!")
+}
